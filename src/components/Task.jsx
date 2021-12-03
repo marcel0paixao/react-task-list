04c1c25a86cb24457efa1ce1e77cd20e9bfeb3a2
marcel0paixao <74371070+marcel0paixao@users.noreply.github.com>
2021-12-03 01:59:55 -0300

    base

---
+++ src/components/Task.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { CgClose, CgInfo } from 'react-icons/cg'
+import { useHistory } from 'react-router';
+
+import './Task.css'
+
+const Task = ({task, handleTaskClick, handleTaskDelete}) => {
+    const history = useHistory()
+
+    const handleTaskInfoClick = () => {
+        history.push(`/${task.title}`)
+    }
+    return (
+        <div className="task-container" style={task.completed ? {borderLeft: '6px solid chartreuse'} : {}}>
+            <div className="task-title" onClick={() => handleTaskClick(task.id)}>
+                {task.title}
+            </div>
+            <div className="buttons-container">
+                <button className="info-task-button" onClick={handleTaskInfoClick}>
+                    <CgInfo/>
+                </button>
+                <button className="remove-task-button" onClick={() => handleTaskDelete(task.id)}>
+                    <CgClose/>
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default Task;
